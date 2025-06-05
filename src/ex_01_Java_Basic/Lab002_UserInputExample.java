7bb139414c1a2d68388bf5af14ee3d54d72dba0d
Acer-nitro-5 <pooja.sharma.te@gmail.com>
2025-06-06 00:12:41 +0530

    New Code push

---
+++ src/ex_01_Java_Basic/Lab002_UserInputExample.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basic;
+
+import java.util.Scanner;
+
+public class Lab002_UserInputExample {
+
+    public static void main(String[] args) {
+        System.out.println("Enter inputs from user");
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter first no.");
+        int a=  sc.nextInt();
+
+        System.out.println("Enter second no.");
+        int b=sc.nextInt();
+
+        System.out.println("sum " + (a+b));
+        sc.close();
+    }
+}
---
+++ src/ex_01_Java_Basic/TablePrint.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class TablePrint {
+
+    public static void main(String[] args) {
+        int n=2;
+        for(int i=1; i<=10; i++){
+          int result=n*i;
+            System.out.println(n + "*" + i + "=" +result);
+        }
+    }
+}
