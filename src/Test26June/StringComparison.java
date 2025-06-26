bb4fc3e65bac27b9b51743288eec9e366923dbe5
Acer-nitro-5 <pooja.sharma.te@gmail.com>
2025-06-26 07:57:40 +0530

    comparison

---
+++ src/Test26June/StringComparison.java
@@ -0,0 +1,26 @@
+package Test26June;
+
+public class StringComparison {
+    public static void main(String[] args) {
+        String s1= "Hello";
+        String s2= "hello";
+        String s3= "Hello";
+
+    //    == operator
+        System.out.println(s1==s2);
+
+        //    equals()
+        System.out.println(s1.equals(s2));
+
+
+        //    equalsIgnoreCase()
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+        //    and compareTo() methods.
+
+        System.out.println(s1.compareTo(s2));
+
+        //    and compareToIgnoreCase() methods.
+        System.out.println(s1.compareToIgnoreCase(s2));
+    }
+}
